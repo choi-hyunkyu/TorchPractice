e191c15f1e0198b2af0dc0913f868209b3ea3465
choi-hyunkyu <69423543+choi-hyunkyu@users.noreply.github.com>
2020-11-07 12:24:41 +0900

    Update scaler.py

--- scaler/scaler.py
+++ scaler/scaler.py
@@ -1,2 +1,15 @@
 def FindMinMax(data):
 	Mindata = np.min(data, 0)
+	Maxdata = np.max(data, 0)
+	
+	return Mindata, Maxdata
+
+def MinMaxScaler(data):
+	numerator = data - np.min(data, 0)
+	denominator = np.max(data, 0) - np.min(data, 0)
+	
+	return numerator / (denominator + 1e-7)
+
+def MinMaxReturn(val, Min, Max):
+	
+	return val * (Max - Min + 1e-7) + Min
